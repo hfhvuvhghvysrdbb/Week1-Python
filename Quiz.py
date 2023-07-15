703846a77738ea101a3e1227a28b2d7de94e76fe
hfhvuvhghvysrdbb <faulcan@gmail.com>
2023-07-15 20:57:44 +1000

    Updated with second part of Quiz

--- Quiz.py
+++ Quiz.py
@@ -1,6 +1,6 @@
 #Quiz
 
-name = "Tristan"
+name = "natsirT"
 print(name)
 
 favouriteNumber = 7
@@ -8,3 +8,15 @@ favouriteNumber = 7
 for i in range (1, favouriteNumber**2 + 1):
     print(i)
 
+class Engineers:
+    def __init__(self, name, type, yearsOfExperience):
+        self.skill = "problem solver"
+        self.name = name
+        self.type = type
+        self.yearsOfExperience = yearsOfExperience
+
+e1 = Engineers("James", "Mechanical", 3)
+e2 = Engineers("Sarah", "Electronics", 2)
+
+print(e1.__dict__)
+print(e2.__dict__)
